7d69d6bb35657eb2efedb80412f4783e247f8823
victorfelipealvarezrojas <victorfelipealvarezrojas@hotmail.com>
2024-01-20 00:33:32 -0300

    edit css add pages add routing

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -1,21 +1,19 @@
 import {  Outlet } from "react-router-dom";
 
+import './dashboardLayout.css'
+
 export const DashboardLayout = () => {
   return (
-    <main className="flex flex-row mt-7">
-      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
-        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
+    <main className="container">
+      <nav className="menu">
+        <h1 className="">
           ReactGPT<span className="text-indigo-500">.</span>
         </h1>
-        <span className="text-xl">Bienvenido</span>
-
-        <div className="border-gray-700 border my-3" />
-
+        <span className="">Bienvenido</span>
         {/* Opciones del menú */}
-
       </nav>
 
-      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+      <section className="content">
         <div className="flex flex-row h-full">
           <div className="flex flex-col flex-auto h-full p-1">
             <Outlet />
